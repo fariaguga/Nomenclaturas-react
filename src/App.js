1da8c1afaed918e05b0c98d1b809f372be60acb1
Gustavo Faria <fariaguga@hotmail.com>
2023-03-08 08:26:58 -0300

    add components and pages,servers,databases,control

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
 import NavBar from "./Components/NavBar";
-import Servers from "./Components/Servers";
-import Databases from "./Components/Databases";
+// import Servers from "./Components/Servers";
+import Servers from "./Pages/Servers";
+import Databases from "./Pages/Databases";
+import HourControl from "./Pages/HourControl";
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Servers />} />
+        <Route path="/" element={<LoginPage/>} />
         <Route path="/servers" element={<Servers />} />
         <Route path="/databases" element={<Databases />} />
+        <Route path="/hour_control" element={<HourControl />} />
+
       </Routes>
     </Router>
   );
